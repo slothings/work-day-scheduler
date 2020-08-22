8d28ac0a49a0d0eac31896dcfb8aeb21ffa0ef37
Slothing <seth.r.martineau@gmail.com>
2020-08-21 22:16:30 -0600

    got my local storage working

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-// display current day/time
+$(document).ready(function () {
 
-$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    // display current day/time
+
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+    // save button function for local storage
+
+    $(".saveBtn").on("click", function () {
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, value);
+
+    })
+
+    // pulling from local storage
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    
+    // changing block colors based on time
+
+    
+
+
+});
