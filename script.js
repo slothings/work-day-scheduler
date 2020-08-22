06fb15fa99b5b9a7e7e4ba79ef25269b0a043587
Slothing <seth.r.martineau@gmail.com>
2020-08-21 21:29:32 -0600

    got my time showing up on script.js

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-var timeBlockTemp = `
-    < div class="row">
-        <div class="col-md-2 hour">12:00AM</div>
-        <div class="time-block col-md-8"><textarea class="time-block></textarea></div>
-    <div class="col-md-2"><button class="saveBtn"></button></div >
-</div >`;
+// display current day/time
 
-var currentDay = document.querySelector("#currentDay");
-
-$(document).ready(function () {
-
-    function date() {
-
-        setInterval(function () {
-            var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-            $("#currentDate").text(currentDate);
-            console.log(currentDate);
-        }, 1000);
-    }
-
-    date();
-
-    for (var i = 0; i < 8; i++) {
-        var timeBlock = $($.parseHTML(timeBlockTemp));
-        $("#timeBlockTemp").append(timeBlock);
-    };
-
-})
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
